3306c2bb7868f98c8040fbb6e88e4ed7fddf24a7
Eric Chapdelaine <chapdelaine.e@northeastern.edu>
2024-08-25 20:13:52 -0400

    add server boilerplate

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const MONGO_URL = "mongodb://mongodb:27017/mind-palace";
+
+const port = 8000;
+
+const CLIENT_URL = "http://localhost:3000"
+
+module.exports = {
+  MONGO_URL,
+  port,
+  CLIENT_URL
+};
