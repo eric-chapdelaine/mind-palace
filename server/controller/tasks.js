3100cbf9ff385635c8862dee6a3cfc5213bd563a
Eric Chapdelaine <emchap4@gmail.com>
2024-08-26 14:44:54 -0400

    Add basic feature endpoints (#1)
    * Added CRD
    
    * Added some delete methods
    
    * Added some delete methods
    
    * Added tag dependency deletion methods
    
    * Added tag dependency deletion methods
    
    * Fixed require statements
    
    * Updated file structure
    
    * Updated file structure
    
    * Added sample data and database removal
    
    * Added README
    
    * Fixed optional fields in create methods
    
    * Fixed behavior with optional fields
    
    * stop tracking node_modules directory
    
    * move server files into server directory
    
    ---------
    
    Co-authored-by: Aaron Agulnick <aaron@agulnick.com>

---
+++ server/controller/tasks.js
@@ -0,0 +1,43 @@
+let Task = require('../models/tasks');
+const { get_activity } = require('./activities');
+const { get_tag } = require('./tags');
+
+exports.get_task = async (title) => {
+    return Task.findOne({'title': title}).populate('activities.activity').populate('tags.tag');
+}
+
+exports.get_all_tasks = async () => {
+    return Task.find({}).populate('activities.activity').populate('tags.tag');
+}
+
+exports.new_task = async (res, title, description, due_date, tag_names, activity_names) => {
+    let task_details = {title: title};
+    if (tag_names != false) {
+        let tags = tag_names.map(function(t) {
+            let tag = await get_tag(t).exec();
+            return tag;
+        });
+        task_details.tags = tags;
+    };
+    if (activity_names != false) {
+    let activities = activity_names.map(function(a) {
+        let activity = await get_activity(a).exec();
+        return activity;
+    });
+    task_details.activities = activities;
+    };
+    if (description == false) {
+        task_details.description = "";
+    } else {
+        task_details.description = description;
+    };
+    if (due_date != false) task_details.due_date = due_date;
+    let task = Task(task_details);
+    await task.save();
+    res.send('Created new task: ' + task);
+    return task;
+}
+
+exports.delete_task = async (title) => {
+    Task.deleteOne({title : title});
+}
--- server/models/schema/tag.js
+++ server/models/schema/tag.js
@@ -4,3 +4,4 @@ module.exports = new mongoose.Schema({
     title: {type: String, unique: true, required: true},
     dependencies: [{tag: {type: mongoose.Schema.Types.ObjectId, ref: "Tag"}}],
 }, {collection: "Tag"});
+// maybe: replace dependencies line with dependencies: {type: [Tag], required: true, default: []} and require Tag from models
