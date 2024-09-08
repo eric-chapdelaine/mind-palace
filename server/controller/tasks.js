305ac3e66f40ee246ceb2167b4592bcc8acee815
Eric Chapdelaine <chapdelaine.e@northeastern.edu>
2024-09-08 19:20:08 -0400

    add basic crud for tasks

--- server/controller/tasks.js
+++ server/controller/tasks.js
@@ -1,43 +1,116 @@
+const express = required("express");
 let Task = require('../models/tasks');
-const { get_activity } = require('./activities');
-const { get_tag } = require('./tags');
 
-exports.get_task = async (title) => {
-    return Task.findOne({'title': title}).populate('activities.activity').populate('tags.tag');
-}
+const router = express.Router();
 
-exports.get_all_tasks = async () => {
-    return Task.find({}).populate('activities.activity').populate('tags.tag');
-}
-
-exports.new_task = async (res, title, description, due_date, tag_names, activity_names) => {
-    let task_details = {title: title};
-    if (tag_names != false) {
-        let tags = tag_names.map(async function(t) {
-            let tag = await get_tag(t).exec();
-            return tag;
+// CRUD
+// create - create new task
+router.post('/', async (req, res) => {
+    try {
+        let {
+            title,
+            due_date,
+            tags,
+            description,
+            activities
+        } = req.body;
+        const task = await Task.create({
+            title,
+            due_date,
+            tags,
+            description,
+            activities
         });
-        task_details.tags = tags;
-    };
-    if (activity_names != false) {
-    let activities = activity_names.map(async function(a) {
-        let activity = await get_activity(a).exec();
-        return activity;
-    });
-    task_details.activities = activities;
-    };
-    if (description == false) {
-        task_details.description = "";
-    } else {
-        task_details.description = description;
-    };
-    if (due_date != false) task_details.due_date = due_date;
-    let task = Task(task_details);
-    await task.save();
-    res.send('Created new task: ' + task);
-    return task;
-}
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
-exports.delete_task = async (title) => {
-    Task.deleteOne({title : title});
-}
+// read - read individual task
+router.get('/:id', async (req, res) => {
+    try {
+        let {id} = req.params;
+        const task = await Task.findOne({_id: id});
+        if (!task) return res.status(404).json({message: "Task not found"});
+        return res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// read - read all tasks
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        if (!tasks) return res.status(500).json({message: "Failed to fetch tasks"});
+        return res.status(200).json
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// update - update task
+router.post('/:id', async (req, res) => {
+    try {
+        let {id} = req.params;
+
+        let {
+            title,
+            due_date,
+            tags,
+            description,
+            activities
+        } = req.body;
+
+        const task = await Task.findOneAndUpdate(
+            {_id: id},
+            {
+                title,
+                due_date,
+                tags,
+                description,
+                activities
+            },
+            {new: true}
+        );
+        if (!task) return res.status(404).json({message: "Task not found"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// update - add tag
+router.post('/:id/add_tag', async (req, res) => {
+    try {
+        // TODO: implement
+        res.status(501).json({message: "Unimplemented"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// update - add activity
+router.post('/:id/add_activity', async (req, res) => {
+    try {
+        // TODO: implement
+        res.status(501).json({message: "Unimplemented"});
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// delete - delete task
+router.delete('/:id', async (req, res) => {
+    try {
+        let {id} = req.params;
+        await Task.deleteOne({_id: id});
+        res.status(200);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+module.exports = router;
