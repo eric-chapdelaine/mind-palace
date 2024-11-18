7e6b2d48dab6e52865a2492181fc3ebfb134bcd6
Eric Chapdelaine <emchap4@gmail.com>
2024-11-17 20:32:43 -0500

    auto fill start/end times for scheduled times (#11)

--- client/src/components/TaskModal/index.js
+++ client/src/components/TaskModal/index.js
@@ -5,6 +5,12 @@ import { updateTask, addScheduledTime, deleteTask } from "../../services/task";
 import { deleteTimeBlock } from "../../services/time_block";
 import { useTasks } from "../../TaskProvider";
 
+const dateToDisplay = (date) => {
+    console.log(date);
+    const newDate = new Date(new Date(date) - 5 * 60 * 60 * 1000);
+    return newDate.toISOString().slice(0, 16);
+}
+
 const TaskModal = ({ task, open, onClose }) => {
 
     const { refreshTasks } = useTasks();
@@ -15,8 +21,8 @@ const TaskModal = ({ task, open, onClose }) => {
     const [isTitleEdit, setTitleEdit] = useState(false);
     const [title, setTitle] = useState(task?.title);
 
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
 
     if (!open) return null;
 
@@ -38,12 +44,12 @@ const TaskModal = ({ task, open, onClose }) => {
             <div className="modal-content" onClick={(e) => { e.stopPropagation(); update(); }}>
                 {isTitleEdit ||
                     <h2 onClick={() => setTitleEdit(true)}>
-                    {task.title}
-                    <button onClick={async () => {
-                        onClose();
-                        await deleteTask(task._id);
-                        await refreshTasks();
-                    }}>delete</button></h2>}
+                        {task.title}
+                        <button onClick={async () => {
+                            onClose();
+                            await deleteTask(task._id);
+                            await refreshTasks();
+                        }}>delete</button></h2>}
 
                 {isTitleEdit &&
                     <><input onClick={(e) => { e.stopPropagation() }}
@@ -56,22 +62,23 @@ const TaskModal = ({ task, open, onClose }) => {
                 Completed:
                 <input type="checkbox"
                     checked={task.is_completed}
-                    onChange={async () => { 
-                    await updateTask(task._id, { is_completed: !task.is_completed });
-                    await refreshTasks();}} />
+                    onChange={async () => {
+                        await updateTask(task._id, { is_completed: !task.is_completed });
+                        await refreshTasks();
+                    }} />
                 <hr />
 
                 {isDescEdit ||
                     <>
-                    <button onClick={() => setDescEdit(true)}>Edit</button> 
-                    <div onClick={() => setDescEdit(true)}>
-                    {task.description.split('\n').map((line, idx) => (
-                        <div key={idx}>
-                        {line}
-                        <br />
+                        <button onClick={() => setDescEdit(true)}>Edit</button>
+                        <div onClick={() => setDescEdit(true)}>
+                            {task.description.split('\n').map((line, idx) => (
+                                <div key={idx}>
+                                    {line}
+                                    <br />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                    </div>
                     </>}
 
                 {isDescEdit &&
@@ -93,9 +100,12 @@ const TaskModal = ({ task, open, onClose }) => {
                 </ul>
 
                 New Scheduled Time:
-                <input type="datetime-local" onChange={(e) => setStartDate(new Date(e.target.value).toISOString())} />
+                <input type="datetime-local" value={dateToDisplay(startDate)} onChange={(e) => {
+                    setStartDate(new Date(e.target.value).toISOString());
+                    setEndDate(new Date(new Date(e.target.value).getTime() + 30 * 60 * 1000).toISOString());
+                }} />
                 -
-                <input type="datetime-local" onChange={(e) => setEndDate(new Date(e.target.value).toISOString())} />
+                <input type="datetime-local" value={dateToDisplay(endDate)} onChange={(e) => setEndDate(new Date(e.target.value).toISOString())} />
                 <button onClick={async () => {
                     await addScheduledTime(task._id, { start_time: startDate, end_time: endDate });
                     await refreshTasks();
