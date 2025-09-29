bd3e1bd01ba0a9fca435e1af44fbefe01b73751a
Eric Chapdelaine <emchap4@gmail.com>
2025-09-28 21:35:44 -0400

    init

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,28 +1,62 @@
-import TaskList from "./TaskList";
 import NewTaskModal from "./NewTaskModal";
 import WeekCalendar from "./WeekCalendar";
 import { useTasks } from "../TaskProvider";
 import { useState, useRef, useEffect } from "react";
-import { displayToday, displayThisWeek, displayEventually, sortByDateCompleted } from "../utils";
+import { displayToday, displayThisWeek, displayEventually, formatDate } from "../utils";
 import "./homepage.css";
 
+// TODO: below component is for testing, once completed, write in the TaskList file
+const TaskList = ({title, tasks}) => {
+  if (!tasks || tasks.length === 0) {
+    return;
+  }
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Status</th>
+          <th>Due Date</th>
+        </tr>
+        {tasks.map((task) => (
+          <tr>
+            <td>{task.title}</td>
+            <td>
+              <input 
+                type="checkbox" 
+                checked={task.is_completed} 
+                readOnly />
+            </td>
+            <td>
+              {formatDate(task.due_date)}
+            </td>
+          </tr>
+        ))}
+      </table>
+      </>
+  )
+}
+
 export default function Homepage() {
-    const { tasks, error } = useTasks();
-    const [newTaskOpen, setNewTaskOpen] = useState(false);
-    const [leftWidth, setLeftWidth] = useState('50%');
-    const homepageRef = useRef(null);
-    const isDragging = useRef(false);
+  const { tasks, error } = useTasks();
+  const [newTaskOpen, setNewTaskOpen] = useState(false);
+  const [leftWidth, setLeftWidth] = useState('50%');
+  const homepageRef = useRef(null);
+  const isDragging = useRef(false);
 
-    const handleMouseDown = () => {
-        isDragging.current = true;
-        document.body.style.cursor = 'col-resize';
-    }
+  const handleMouseDown = () => {
+    isDragging.current = true;
+    document.body.style.cursor = 'col-resize';
+  }
 
-     const handleMouseMove = (e) => {
+  const handleMouseMove = (e) => {
     if (!isDragging.current || !homepageRef.current) return;
 
     const containerRect = homepageRef.current.getBoundingClientRect();
     const newLeftWidth = ((e.clientX - containerRect.left) / containerRect.width) * 100;
+    console.log(newLeftWidth);
 
     // Set a minimum and maximum width for the panels
     if (newLeftWidth > 10 && newLeftWidth < 90) {
@@ -44,28 +78,24 @@ export default function Homepage() {
       document.removeEventListener('mouseup', handleMouseUp);
     };
   }, []);
+  
+  if (error) return (
+    <>Error occured: {error.message}</>
+  );
 
-    if (error) return (
-        <>Error occured: {error.message}</>
-    );
-
-
-    return (
-        <div className="homepage" ref={homepageRef}>
+  return (
+    <div className="homepage" ref={homepageRef}>
             <div className="left-panel" style={{ flexBasis: leftWidth }}>
-                <button style={{ position: "fixed" }} onClick={() => setNewTaskOpen(true)}> New Task </button>
-                <NewTaskModal open={newTaskOpen} onClose={() => setNewTaskOpen(false)} />
-                <div style={{display: "flex", marginTop: "25px"}}>
-                    <TaskList title="Today" tasks={tasks.filter(displayToday)} />
-                    <TaskList title="This Week" tasks={tasks.filter(displayThisWeek)} />
-                    <TaskList title="Eventually" tasks={tasks.filter(displayEventually)} />
-                    <TaskList title="Completed" tasks={tasks.filter((t) => t.is_completed).sort(sortByDateCompleted)} />
-                </div>
-            </div>
+      <button onClick={() => setNewTaskOpen(true)}> New Task </button>
+      <NewTaskModal open={newTaskOpen} onClose={() => setNewTaskOpen(false)} />
+      <TaskList title={"Today"} tasks={tasks.filter(displayToday)} />
+      <TaskList title={"This Week"} tasks={tasks.filter(displayThisWeek)} />
+      <TaskList title={"Eventually"} tasks={tasks.filter(displayEventually)} />
+      </div>
         <div className="divider" onMouseDown={handleMouseDown}> </div>
             <div className="right-panel" style={{ flexBasis: `calc(100% - ${leftWidth} - 5px` }}>
                 <WeekCalendar tasks={tasks} />
             </div>
-        </div>
-    )
+    </div>
+  )
 }
